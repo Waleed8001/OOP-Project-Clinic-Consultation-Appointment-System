0bf4b545885f12d1078231fffecb2866e2fb8456
Muhammad Waleed Kamal <157250005+Waleed8001@users.noreply.github.com>
2024-06-29 23:21:30 +0500

    Update Form3.cs

--- Form3.cs
+++ Form3.cs
@@ -1,4 +1,4 @@
-﻿/*sing MySql.Data.MySqlClient;
+﻿using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -7,65 +7,6 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace OOP_s_Project_Clinic_Consultation_Appointment_System
-{
-    public partial class Form3 : Form
-    {
-        public Form3()
-        {
-            InitializeComponent();
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            if (textBox4.Text != "" && textBox3.Text != "")
-            {
-                string con = "server=localhost;uid=root;pwd=W#0324@aleed;database=loginids";
-                MySqlConnection c = new MySqlConnection();
-                c.ConnectionString = con;
-                c.Open();
-                string query = "SELECT * FROM loginids.ids WHERE Email = @email AND Password = @pass";
-                MySqlCommand cmd = new MySqlCommand(query, c);
-                cmd.Parameters.AddWithValue("@email", textBox4.Text);
-                cmd.Parameters.AddWithValue("@pass", textBox3.Text);
-
-                MySqlDataReader dr = cmd.ExecuteReader();
-                if (dr.HasRows == true)
-                {
-                    MessageBox.Show("Login Successfull", "Login");
-                    textBox3.Text = "";
-                    textBox4.Text = "";
-                }
-                else
-                {
-                    MessageBox.Show("Login Failed", "Failed");
-                    textBox3.Text = "";
-                    textBox4.Text = "";
-                }
-                c.Close();
-                Form5 f5 = new Form5();
-                f5.Show();
-            }
-            else
-            {
-                MessageBox.Show("Please fill all fields!!!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                textBox3.Text = "";
-                textBox4.Text = "";
-            }
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            Form3.ActiveForm.Close();
-        }
-    }
-}
-*/
-
-using MySql.Data.MySqlClient;
-using System;
 using System.Windows.Forms;
 
 namespace OOP_s_Project_Clinic_Consultation_Appointment_System
