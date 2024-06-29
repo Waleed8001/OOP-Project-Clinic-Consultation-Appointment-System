dce6bee79595e1d51ec1c949c73f7ef21771d59f
Muhammad Waleed Kamal <157250005+Waleed8001@users.noreply.github.com>
2024-06-29 23:17:48 +0500

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -9,33 +9,6 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
 
-/*namespace OOP_s_Project_Clinic_Consultation_Appointment_System
-{
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-            Form2 f2 = new Form2();
-            f2.Show();
-        }
-    }
-}*/
-/*sing System;
-using System.Windows.Forms;
-using MySql.Data.MySqlClient;*/
-
-
-
 namespace OOP_s_Project_Clinic_Consultation_Appointment_System
 {
     public partial class Form1 : BaseForm
