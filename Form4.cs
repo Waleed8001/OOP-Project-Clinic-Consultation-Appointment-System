adf4e0693f083903bb0e7da5c959c0c62579753f
Muhammad Waleed Kamal <157250005+Waleed8001@users.noreply.github.com>
2024-06-29 23:22:36 +0500

    Update Form4.cs

--- Form4.cs
+++ Form4.cs
@@ -1,4 +1,4 @@
-﻿/*using MySql.Data.MySqlClient;
+﻿using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -7,69 +7,6 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace OOP_s_Project_Clinic_Consultation_Appointment_System
-{
-    public partial class Form4 : Form
-    {
-        public Form4()
-        {
-            InitializeComponent();
-        }
-
-        private void button5_Click(object sender, EventArgs e)
-        {
-            Form4.ActiveForm.Close();
-        }
-
-        private void button6_Click(object sender, EventArgs e)
-        {
-            if(textBox5.Text != "" && textBox1.Text != "" && textBox2.Text != "")
-            {
-                string con = "server=localhost;uid=root;pwd=W#0324@aleed;database=loginids";
-                MySqlConnection c = new MySqlConnection();
-                c.ConnectionString = con;
-                c.Open();
-                string query = "INSERT INTO ids VALUES(@username, @password, @email)";
-                MySqlCommand cmd = new MySqlCommand(query, c);
-                cmd.Parameters.AddWithValue("@username", textBox5.Text);
-                cmd.Parameters.AddWithValue("@password", textBox2.Text);
-                cmd.Parameters.AddWithValue("@email", textBox1.Text);
-                int a = cmd.ExecuteNonQuery();
-                if (a > 0)
-                {
-                    MessageBox.Show("Data Inserted");
-                    textBox5.Text = "";
-                    textBox2.Text = "";
-                    textBox1.Text = "";
-                }
-                else
-                {
-                    MessageBox.Show("Data is not inserted");
-                    textBox5.Text = "";
-                    textBox2.Text = "";
-                    textBox1.Text = "";
-                }
-                c.Close();
-                Form5 f5 = new Form5();
-                f5.Show();
-            }
-            else
-            {
-                MessageBox.Show("Please fill all fields!!!","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
-                textBox5.Text = "";
-                textBox2.Text = "";
-                textBox1.Text = "";
-            }
-            
-        }
-    }
-}*/
-
-
-using MySql.Data.MySqlClient;
-using System;
 using System.Windows.Forms;
 
 namespace OOP_s_Project_Clinic_Consultation_Appointment_System
